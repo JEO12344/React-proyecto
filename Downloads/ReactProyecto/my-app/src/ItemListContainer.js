5a67304c5182f73b57393e5ccdbb9d0591ed3162
Juan Emilio <ortegarisso@gmail.com>
2024-02-01 10:39:18 -0300

    segundo commit

--- Downloads/ReactProyecto/my-app/src/ItemListContainer.js
+++ Downloads/ReactProyecto/my-app/src/ItemListContainer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import libro1 from "./components/img/libroHobbit.jpg";
+import libro2 from "./components/img/libroPeter.png";
+import libro3 from "./components/img/libroVueltaAlMundo.jpg";
 
 const ItemListContainer = ({ greeting }) => {
   return (
     <div className="item-list-container">
-      <p>{greeting}</p>
+      <h1>{greeting}</h1>
+      <div className="libro">
+        <img src={libro1} alt="Libro 1" />
+        <p></p>
+      </div>
+      <div className="libro">
+        <img src={libro2} alt="Libro 2" />
+        <p></p>
+      </div>
     </div>
   );
 };
